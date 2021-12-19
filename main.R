01dc92c1f33d6cfe51ddf323f555b8b3862ab497
Randeep <randeep@pop-os.localdomain>
2021-12-19 14:41:48 +0530

    added code to convert appId to name

--- main.R
+++ main.R
@@ -1,14 +1,26 @@
 library(RSQLite)
+library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(readr)
 
 # Read SQLite file and fetch all data from usageStats Table
 con <- dbConnect(drv=RSQLite::SQLite(), dbname="usageDirect-history.sqlite3")
 usage_stats <- dbGetQuery(conn=con, statement=paste("SELECT * FROM 'usageStats'", sep=""))
-
+usage_stats <- as_tibble(usage_stats)
+                         
 # table contains days since epoch, converting it to date format for further use
-usage_stats <- usage_stats %>% mutate(date= as.Date("1970-01-01") + day)
-# Add timeUsed in secs column 
-usage_stats <- usage_stats %>% mutate(timeUsed_secs = timeUsed/1000)
+usage_stats <- usage_stats %>%
+               mutate(date= as_date(day),timeUsed_secs = timeUsed/1000) %>%
+               select(applicationId, date, timeUsed_secs) 
+
+# Reading app_name that contains map of ApplicationId to app names,
+# If app_names doesn't contain application_id then use ApplicationId
+app_names <- read_csv("app_names.csv")
+usage_stats_with_names <- usage_stats %>% 
+                          left_join(app_names, by=c('applicationId' = 'PackageName')) %>%
+                          mutate(AppName = if_else(is.na(AppName),applicationId,AppName))
+
+
 
 
