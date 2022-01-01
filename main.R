7c47400c3658e6201eb7e95de5d4a0eea02f3e77
Randeep <randeep.saini07@gmail.com>
2022-01-01 17:19:49 +0530

    Added geom_point plots

--- main.R
+++ main.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 
+# Create direcotry for plots to be saved
+dir.create(file.path('plots'), showWarnings = FALSE)
+
 # Read SQLite file and fetch all data from usageStats Table
 con <- dbConnect(drv=RSQLite::SQLite(), dbname="data/usageDirect-history.sqlite3")
 usage_stats <- dbGetQuery(conn=con, statement=paste("SELECT * FROM 'usageStats'", sep=""))
@@ -42,13 +45,13 @@ usage_stats_with_names %>%
   ggplot(aes(date, total_hours)) + geom_point(aes(color=is_weekend))
 ggsave('plots/daily_total_usage_scatter.jpg')
 
-# Total and Average hours plot of app Usage on different Weekdays
+# Total hours plot of app Usage on different Weekdays
 usage_stats_with_names %>% 
   group_by(w_day) %>%
   summarise(total_hours=sum(timeUsed_secs/60/60)) %>% 
   ggplot(aes(w_day, total_hours)) + geom_col()
 
-
+# Average hours plot of app Usage on different Weekdays
 usage_stats_with_names %>% 
   group_by(date, w_day) %>%
   summarise(total_hours=sum(timeUsed_secs/60/60)) %>%
@@ -79,7 +82,7 @@ usage_stats_with_names %>%
   ) 
 ggsave('plots/consistently_used_daily_top5_apps.jpg')
 
-## Filter out most used App of every day and count them
+## Filter out most used App of every day and plot them
 usage_stats_with_names %>% 
   group_by(date) %>% 
   filter(timeUsed_secs == max(timeUsed_secs)) %>% 
@@ -95,4 +98,38 @@ usage_stats_with_names %>%
     y = "No. of Times App is Most Used in a Day"
   ) 
 ggsave('plots/mostly_used_apps_in_a_day.jpg')
+
+## Filter out most used App of every day and plot with timeUsed as size
+usage_stats_with_names %>% 
+  group_by(date) %>% 
+  filter(timeUsed_secs == max(timeUsed_secs)) %>% 
+  group_by(AppName) %>% 
+  summarize(n=n(), total_app_used_hrs=sum(timeUsed_secs)/60/60) %>%
+  arrange(desc(n)) %>% 
+  head(10) %>%
+  ggplot(aes(n,reorder(AppName, n))) + 
+  geom_point(aes(size=total_app_used_hrs)) +
+  labs(
+    x = "App Name",
+    y = "No. of Times App is Most Used in a Day"
+  ) 
+ggsave('plots/mostly_used_apps_in_a_day_2.jpg')
+
+# Filter Top 5 Apps of everyday and count which apps are
+# in top 5 for most of the days geom_point with timeUsed as size
+usage_stats_with_names %>% 
+  group_by(date) %>% 
+  filter(rank(desc(timeUsed_secs)) < 5) %>% 
+  group_by(AppName) %>% 
+  summarize(n=n(), total_app_used_hrs=sum(timeUsed_secs)/60/60) %>%
+  arrange(desc(n)) %>% 
+  head(10) %>%
+  ggplot(aes(n,reorder(AppName, n))) + 
+  geom_point(aes(size=total_app_used_hrs)) +
+  labs(
+    x = "App Name",
+    y = "No. of Times App is in Daily Top 5",
+    title = "Most Consistently used Apps"
+  ) 
+ggsave('plots/consistently_used_daily_top5_apps_2.jpg')
 
